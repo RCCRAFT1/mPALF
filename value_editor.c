081eea5ea04bf0904c2ec5986eacba03361537d6
RCCRAFT1 <43323661+RCCRAFT1@users.noreply.github.com>
2021-07-26 20:46:27 -0700

    Add files via upload

---
+++ value_editor.c
@@ -0,0 +1,11 @@
+@value.getInterface //use this exact line for any feature you'd like to add
+// here is an example edit
+
+
+
+//@value.getInterface
+//interface.pluginZeroDay
+//interface.getFromServer('')
+//type == interface.file(any)
+
+
